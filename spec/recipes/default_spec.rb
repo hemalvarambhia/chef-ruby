296a39a850dc7d08bdedf9eeebae24889bd3e066
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-12-09 17:22:57 +0000

    first failing spec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -4,8 +4,15 @@ describe 'ruby::default' do
   context "on ubuntu" do
     let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-    it "does nothing" do
-
+    it "installs dependencies" do
+      [
+          "openssl", "libreadline6", "libreadline6-dev",
+          "zlib1g", "zlib1g-dev", "libssl-dev",
+          "libyaml-dev", "libxml2-dev", "libxslt-dev",
+          "libc6-dev", "ncurses-dev", "libtool"
+      ].each { |dependency|
+        expect(chef_run).to install_package dependency
+      }
     end
   end
 end
