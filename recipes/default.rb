91763b94eaaa903d6625e91e254bb4a0e32d74dd
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-12-24 17:40:59 +0000

    use apt to update repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,7 @@
 #
 case node.platform_family
   when "debian"
+    include_recipe "apt::default"
     include_recipe "build-essential::default"
     node[:ruby][:dependencies].each { |dependency|
       package dependency do
--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -5,6 +5,10 @@ describe 'ruby::default' do
     let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
     it "updates apt repo" do
+      expect(chef_run).to include_recipe "apt::default"
+    end
+
+    it "installs package for compiling C code" do
       expect(chef_run).to include_recipe "build-essential::default"
     end
 
