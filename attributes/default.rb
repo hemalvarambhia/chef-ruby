78b143a813746b4581480f015e2e0b93ab4ada44
Hemal <h.n.varambhia@gmail.com>
2014-12-29 12:36:13 +0000

    use the node attribute that stores dependencies for each platform

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,18 @@
-default[:ruby][:dependencies] = [
-    "openssl", "libreadline6", "libreadline6-dev",
-    "zlib1g", "zlib1g-dev", "libssl-dev",
-    "libyaml-dev", "libxml2-dev", "libxslt-dev",
-    "libc6-dev", "ncurses-dev", "libtool"
-]
+case node.platform_family
+    when 'debian'
+        default[:ruby][:dependencies] = [
+            "openssl", "libreadline6", "libreadline6-dev",
+            "zlib1g", "zlib1g-dev", "libssl-dev",
+            "libyaml-dev", "libxml2-dev", "libxslt-dev",
+            "libc6-dev", "ncurses-dev", "libtool"
+        ]
+
+    when 'rhel'
+        default[:ruby][:dependencies] = [
+            "readline", "readline-devel", "zlib", "zlib-devel",
+            "libyaml-devel", "libffi-devel", "bzip2", "libtool",
+            "openssl", "openssl-devel", "libxml2",
+            "libxml2-devel", "libxslt", "libxslt-devel"
+        ]
+
+end
