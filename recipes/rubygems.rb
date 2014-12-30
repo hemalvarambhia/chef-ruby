d6fa180651b5ba1a46df7d59e16ecc71155008c0
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-12-30 15:41:20 +0000

    untar the rubygems tar ball

--- recipes/rubygems.rb
+++ recipes/rubygems.rb
@@ -4,3 +4,8 @@ remote_file rubygems_source_tarball do
   source "http://production.cf.rubygems.org/rubygems/#{rubygems_source_tarball}"
   action :create
 end
+
+execute("untar-rubygems-tarball") do
+  command "tar -zxf #{rubygems_source_tarball} -C #{node[:ruby][:installation_dir]}"
+  action :run
+end
