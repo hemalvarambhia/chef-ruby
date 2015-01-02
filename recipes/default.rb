6e25292ab27dbc6b565ffa63c852c1b93a3cacda
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-01-02 16:49:47 +0000

    only install ruby if the version is not already installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+Chef::Resource::RemoteFile.send(:include, ChefRuby::Helper)
+Chef::Resource::Execute.send(:include, ChefRuby::Helper)
 
 case node.platform_family
   when "debian"
@@ -28,17 +30,20 @@ ruby_tar_ball = "ruby-#{node[:ruby][:version]}.tar.gz"
 source_code_site = "http://ftp.ruby-lang.org/pub/ruby/#{node[:ruby][:version][0..2]}/#{ruby_tar_ball}"
 remote_file(ruby_tar_ball) do
   source source_code_site
+  not_if already_installed?
   action :create
 end
 
 execute("untar-ruby-source-code") do
   command "tar -zxf #{ruby_tar_ball} -C #{node[:ruby][:src_dir]}"
+  not_if already_installed?
   action :run
 end
 
 execute("compile-ruby-#{node[:ruby][:version]}") do
   cwd "#{node[:ruby][:src_dir]}/ruby-#{node[:ruby][:version]}"
   command "autoconf && ./configure && make && make install"
+  not_if already_installed?
   action :run
 end
 
