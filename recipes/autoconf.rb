d7051c78c116834ec49abff42ff0aed7f30d1f69
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-01-02 10:16:28 +0000

    go back to getting latest version

--- recipes/autoconf.rb
+++ recipes/autoconf.rb
@@ -1,5 +1,5 @@
 
-autoconf_version = "2.67"
+autoconf_version = "2.69"
 autoconf_tarball = "autoconf-#{autoconf_version}.tar.gz"
 remote_file autoconf_tarball do
   source "http://ftp.gnu.org/gnu/autoconf/#{autoconf_tarball}"
--- spec/recipes/autoconf_spec.rb
+++ spec/recipes/autoconf_spec.rb
@@ -3,16 +3,16 @@ require_relative '../spec_helper'
 describe "chef-ruby::autoconf" do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-  it "downloads autoconf source code version >=2.60" do
-    expect(chef_run).to create_remote_file("autoconf-2.67.tar.gz").with(
-                            source: "http://ftp.gnu.org/gnu/autoconf/autoconf-2.67.tar.gz"
+  it "downloads the latest autoconf source code version >=2.60" do
+    expect(chef_run).to create_remote_file("autoconf-2.69.tar.gz").with(
+                            source: "http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
                         )
   end
 
   it "installs autoconf" do
     expect(chef_run).to run_execute("build-autoconf").with(
                             command: "./configure --prefix=/usr && make && make install",
-                            cwd: "#{chef_run.node[:ruby][:src_dir]}/autoconf-2.67"
+                            cwd: "#{chef_run.node[:ruby][:src_dir]}/autoconf-2.69"
                         )
   end
 end
