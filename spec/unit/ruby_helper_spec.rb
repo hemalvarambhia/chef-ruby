49d84f0fb02fdf9d57ab113e1efd15610f3a9b5f
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-01-02 15:11:46 +0000

    check that when different version and patch is already installed, required one is not

--- spec/unit/ruby_helper_spec.rb
+++ spec/unit/ruby_helper_spec.rb
@@ -21,7 +21,30 @@ describe ChefRuby::Helper do
       expect(shellout).to receive(:live_stream=).and_return(nil)
       client_class.new.already_installed?
     end
-    
+
+    describe "different version installed" do
+      let(:shellout) { double(run_command: nil, error!: nil, stdout: "1.8.7-p358", stderr: double(empty?: true)) }
+      let(:client_class) {
+        Class.new {
+          include ChefRuby::Helper
+
+          def node
+            {ruby: {version: "1.9.2-p320"}}
+          end
+        }
+      }
+
+      before :each do
+        Mixlib::ShellOut.stub(:new).and_return(shellout)
+      end
+
+      it "confirms the required version is not installed" do
+        expect(shellout).to receive(:live_stream=).and_return(nil)
+
+        expect(client_class.new.already_installed?).to eq(false)
+      end
+    end
+
     describe "same version of ruby is installed" do
       let(:client_class) {
         Class.new {
