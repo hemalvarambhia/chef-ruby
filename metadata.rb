6e81e3b7b616444b22e670de75a0911ed16df28f
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-01-02 13:34:54 +0000

    corrected recipe name in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'ruby'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'chef-ruby'
+maintainer       'Software Craftsman of London'
+maintainer_email 'hemal@softwarecraftsman.co.uk'
 license          'All rights reserved'
-description      'Installs/Configures ruby'
+description      'Installs ruby from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends "apt"
