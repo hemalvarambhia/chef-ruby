1da89450412ecfdfea2ef14d5d3bd9dfd4ee4ec4
Hemal <h.n.varambhia@gmail.com>
2015-01-27 16:57:46 +0000

    serverspec test for ruby-2.0.0-p353 suite

---
+++ test/integration/ruby-2.0.0-p353/serverspec/default_spec.rb
@@ -0,0 +1,12 @@
+require 'serverspec'
+
+set :backend, :exec
+set :path, '/bin:/usr/local/bin:$PATH'
+
+describe "installing ruby" do
+  describe command("ruby -v") do
+    its(:stdout) {
+      should match "ruby 2.0.0p353"
+    }
+  end
+end
