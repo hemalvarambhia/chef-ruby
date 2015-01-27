e9502571654dd222077887192357d42f4783fdf4
Hemal <h.n.varambhia@gmail.com>
2015-01-27 16:22:55 +0000

    Add /usr/local/bin to the PATH environment and then just run ruby -v

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,10 @@
 require 'serverspec'
 
 set :backend, :exec
+set :path, '/bin:/usr/local/bin:$PATH'
 
 describe "installing ruby" do
-  describe command("/usr/local/bin/ruby -v") do
+  describe command("ruby -v") do
     its(:stdout) {
       should match "ruby 1.9.2p320"
     }
