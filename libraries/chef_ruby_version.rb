20c89233e462dead19d9737b8007ee2eeb07ca45
Hemal <h.n.varambhia@gmail.com>
2015-02-06 18:04:15 +0000

    Revert "Use the Version object now"
    This reverts commit df4a9a1a663e0b366fc1787be041a0dbb15bfaa1.

--- libraries/chef_ruby_version.rb
+++ libraries/chef_ruby_version.rb
@@ -6,10 +6,6 @@ module ChefRuby
       raise Exception.new("This version is invalid") if version.split(".").size != 3
       @version = version
     end
-   
-    def [](*indices)
-      @version[indices]
-    end
 
     def <=>(other)
       @version <=> other.version
