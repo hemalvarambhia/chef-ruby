62d087947ae4669f0db4b57d5a05bbd6c499a0b3
Hemal <h.n.varambhia@gmail.com>
2015-03-10 15:32:01 +0000

    Check the ruby & gem binaries exist in /usr/local/bin, call them if they do, improved readability

--- libraries/ruby_helper.rb
+++ libraries/ruby_helper.rb
@@ -3,12 +3,14 @@ module ChefRuby
     include Chef::Mixin::ShellOut
 
     def already_installed?
-      command = shell_out("ruby -v",  {returns: [0, 2]})
+      ruby_binary = "#{node[:ruby][:bin_dir]}/ruby"
+      ruby_installed_command = shell_out("[ -x #{ruby_binary} ] && #{ruby_binary} -v",  {returns: [0, 2]})
       expected_version = Regexp.escape(node[:ruby][:version].gsub("-", ""))
       ruby_18x_version_output = /#{node[:ruby][:version][0..4]} \((?:\d{4}-\d{2}-\d{2}) patchlevel #{patch_level}\)/
       ruby_version_output = /#{expected_version}/
-      command.stderr.empty? and !(command.stdout=~ruby_version_output).nil? or
-          !(command.stdout=~ruby_18x_version_output).nil?
+
+      ruby_installed_command.stderr.empty? and !(ruby_installed_command.stdout.strip=~ruby_version_output).nil? or
+          !(ruby_installed_command.stdout.strip=~ruby_18x_version_output).nil?
     end
 
     def patch_level
@@ -22,9 +24,11 @@ module ChefRuby
     end
 
     def rubygems_already_installed?
-      command = shell_out("gem -v", {returns: [0, 2]})
+      gem_binary = "#{node[:ruby][:bin_dir]}/gem"
+      rubygems_installed_command = shell_out("[ -x #{gem_binary} ] && #{gem_binary} -v", {returns: [0, 2]})
 
-      command.stderr.empty? and command.stdout == node[:ruby][:rubygems_version]
+      rubygems_installed_command.stderr.empty? and
+          rubygems_installed_command.stdout.strip == node[:ruby][:rubygems_version]
     end
 
     def patch_not_already_applied?
--- spec/unit/ruby_helper_spec.rb
+++ spec/unit/ruby_helper_spec.rb
@@ -5,7 +5,7 @@ describe ChefRuby::Helper do
       include ChefRuby::Helper
 
       def node
-        {ruby: {version: "1.9.2-p320"}}
+        {ruby: {version: "1.9.2-p320", bin_dir: "/usr/local/bin"} }
       end
     }
   }
@@ -18,12 +18,26 @@ describe ChefRuby::Helper do
     end
 
     it "builds the correct command" do
-      expect(Mixlib::ShellOut).to receive(:new).with('ruby -v', {:returns=>[0, 2]})
+      expect(Mixlib::ShellOut).to receive(:new).with('[ -x /usr/local/bin/ruby ] && /usr/local/bin/ruby -v', {:returns=>[0, 2]})
       expect(shellout).to receive(:live_stream=).and_return(nil)
       client_class.new.already_installed?
     end
 
-    describe "different version installed" do
+    context "when ruby is not installed" do
+      let(:shellout) { double(run_command: nil, error!: nil, stdout: "", stderr: double(empty?: true)) }
+
+      before :each do
+        Mixlib::ShellOut.stub(:new).and_return(shellout)
+      end
+
+      it "confirms the required version is not installed" do
+        expect(shellout).to receive(:live_stream=).and_return(nil)
+
+        expect(client_class.new.already_installed?).to eq(false)
+      end
+    end
+
+    context "when different version installed" do
       let(:shellout) { double(run_command: nil, error!: nil, stdout: "ruby 1.8.7 (2013-06-27 patchlevel 358) [x86_64-linux]", stderr: double(empty?: true)) }
 
       before :each do
@@ -37,8 +51,8 @@ describe ChefRuby::Helper do
       end
     end
 
-    describe "same version of ruby is installed" do
-      context "different patch level" do
+    context "when same version of ruby is installed" do
+      context "when patch level is different" do
         let(:shellout) { double(run_command: nil, error!: nil, stdout: 'ruby 1.9.2p321 (2012-04-20 revision 35421) [x86_64-linux]', stderr: double(empty?: true)) }
 
         before :each do
@@ -52,7 +66,7 @@ describe ChefRuby::Helper do
         end
       end
 
-      context "same patch level" do
+      context "when the patch level is the same" do
         let(:shellout) { double(run_command: nil, error!: nil, stdout: 'ruby 1.9.2p320 (2012-04-20 revision 35421) [x86_64-linux]', stderr: double(empty?: true)) }
 
         before :each do
@@ -67,13 +81,13 @@ describe ChefRuby::Helper do
       end
     end
 
-    describe "ruby 1.8 series" do
+    context "ruby 1.8 series" do
       let(:client_class) {
         Class.new {
           include ChefRuby::Helper
 
           def node
-            {ruby: {version: "1.8.7-p358"}}
+            {ruby: {version: "1.8.7-p358", bin_dir: "/usr/local/bin"}}
           end
         }
       }
