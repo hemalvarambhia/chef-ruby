2cb3e51e0ce52966dd2abe618ff0caa92db4ae0c
Hemal <h.n.varambhia@gmail.com>
2015-06-08 15:25:13 +0100

    Versions updated to reflect ruby-lang.org

--- test/integration/current_stable/serverspec/default_spec.rb
+++ test/integration/current_stable/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "installing ruby" do
   describe command("ruby -v") do
     its(:stdout) {
-      should match "ruby 2.2.1"
+      should match "ruby 2.2.2"
     }
   end
 end
--- test/integration/previous_stable/serverspec/default_spec.rb
+++ test/integration/previous_stable/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "installing ruby" do
   describe command("ruby -v") do
     its(:stdout) {
-      should match "ruby 2.1.5"
+      should match "ruby 2.1.6"
     }
   end
 end
