eed060efa00c8e0ca3d5a198752abaddd15a5a12
Hemal <h.n.varambhia@gmail.com>
2017-02-13 11:56:11 +0000

    Improved code formatting.

--- spec/recipes/autoconf_spec.rb
+++ spec/recipes/autoconf_spec.rb
@@ -4,28 +4,35 @@ describe "chef-ruby::autoconf" do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
   context "when autoconf is not already installed" do
     before :each do
-      Chef::Resource::RemoteFile.any_instance.stub(:autoconf_already_installed?).with('2.69').and_return(false)
-      Chef::Resource::Execute.any_instance.stub(:autoconf_already_installed?).with('2.69').and_return(false)
+      Chef::Resource::RemoteFile.any_instance.
+          stub(:autoconf_already_installed?).with('2.69').and_return(false)
+      Chef::Resource::Execute.any_instance.
+          stub(:autoconf_already_installed?).with('2.69').and_return(false)
     end
 
     it "downloads the latest autoconf source code version >=2.60" do
-      expect(chef_run).to create_remote_file("autoconf-2.69.tar.gz").with(
-                              source: "http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
-                          )
+      expect(chef_run).to(
+          create_remote_file("autoconf-2.69.tar.gz").with(
+              source: "http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz")
+      )
     end
 
     it "installs autoconf" do
-      expect(chef_run).to run_execute("build-autoconf").with(
-                              command: "./configure --prefix=/usr && make && make install",
-                              cwd: "#{chef_run.node[:ruby][:src_dir]}/autoconf-2.69"
-                          )
+      expect(chef_run).to(
+          run_execute("build-autoconf").with(
+              command: "./configure --prefix=/usr && make && make install",
+              cwd: "#{chef_run.node[:ruby][:src_dir]}/autoconf-2.69"
+          )
+      )
     end
   end
 
   context "when autoconf is installed" do
     before :each do
-      Chef::Resource::RemoteFile.any_instance.stub(:autoconf_already_installed?).with('2.69').and_return(true)
-      Chef::Resource::Execute.any_instance.stub(:autoconf_already_installed?).with('2.69').and_return(true)
+      Chef::Resource::RemoteFile.any_instance.
+          stub(:autoconf_already_installed?).with('2.69').and_return(true)
+      Chef::Resource::Execute.any_instance.
+          stub(:autoconf_already_installed?).with('2.69').and_return(true)
     end
 
     it "downloads the latest autoconf source code version >=2.60" do
--- spec/recipes/rubygems_spec.rb
+++ spec/recipes/rubygems_spec.rb
@@ -4,15 +4,20 @@ describe 'chef-ruby::rubygems' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   before :each do
-    Chef::Resource::RemoteFile.any_instance.stub(:rubygems_already_installed?).and_return(false)
-    Chef::Resource::Execute.any_instance.stub(:rubygems_already_installed?).and_return(false)
+    Chef::Resource::RemoteFile.any_instance.
+        stub(:rubygems_already_installed?).and_return(false)
+    Chef::Resource::Execute.any_instance.
+        stub(:rubygems_already_installed?).and_return(false)
   end
 
   context "when rubygems is not already installed" do
     it "downloads the rubygems source code" do
-      expect(chef_run).to create_remote_file("rubygems-2.6.10.tgz").with(
-                              source: "http://production.cf.rubygems.org/rubygems/rubygems-2.6.10.tgz"
-                          )
+      expect(chef_run).to(
+          create_remote_file("rubygems-2.6.10.tgz").with(
+              source: "http://production.cf.rubygems.org/rubygems/rubygems-2.6.10.tgz"
+
+          )
+      )
     end
 
     it "installs rubygems" do
@@ -22,15 +27,19 @@ describe 'chef-ruby::rubygems' do
 
   context "when rubygems is already installed" do
     it "does not download the rubygems source" do
-      Chef::Resource::RemoteFile.any_instance.stub(:rubygems_already_installed?).and_return true
+      Chef::Resource::RemoteFile.any_instance.
+          stub(:rubygems_already_installed?).and_return true
 
-      expect(chef_run).to_not create_remote_file("rubygems-2.6.10.tgz").with(
-                              source: "http://production.cf.rubygems.org/rubygems/rubygems-2.6.10.tgz"
-                          )
+      expect(chef_run).to_not(
+          create_remote_file("rubygems-2.6.10.tgz").with(
+              source: "http://production.cf.rubygems.org/rubygems/rubygems-2.6.10.tgz"
+          )
+      )
     end
 
     it "does not install rubygems" do
-      Chef::Resource::Execute.any_instance.stub(:rubygems_already_installed?).and_return true
+      Chef::Resource::Execute.any_instance.
+          stub(:rubygems_already_installed?).and_return true
 
       expect(chef_run).to_not install_rubygems('2.6.10')
     end
